8d83999b0c22843fbd77de2956d407912d0087c9
Michelle Gesaka <user@172.16.34.131>
2026-07-26 14:29:40 +0300

    Use environment variables for DB

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "root";
-$database = "lost_and_found";
-$port = 8889;
+$host = getenv('DB_HOST') ?: 'reseau.proxy.rlwy.net';
+$user = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASS') ?: 'jRLMkLYNVQZQsjLzJScVrXTjBFtHGdob';
+$database = getenv('DB_NAME') ?: 'railway';
+$port = getenv('DB_PORT') ?: 29877;
 
 $conn = mysqli_connect($host, $user, $password, $database, $port);
 
